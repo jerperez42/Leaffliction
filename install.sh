2bfc2cf3818fb069b1b489808243688aeed7d8a1
Jeremy Perez <jerperez@paul-f4Ar5s7.clusters.42paris.fr>
2025-04-28 10:07:41 +0200

    venv

---
+++ install.sh
@@ -0,0 +1,6 @@
+python3 -m venv ~/goinfre/.venv
+source ~/goinfre/.venv/bin/activate
+pip install flake8
+pip install --no-cache-dir -r requirements.txt
+
+echo 'source ~/goinfre/.venv/bin/activate'
